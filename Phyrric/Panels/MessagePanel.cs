306b186dac699aee74bd2c90757338bfa9bb8ff4
Andy Maesen <maesenandy@gmail.com>
2016-07-21 00:22:29 +0200

    Removed not used methods

--- Phyrric/Panels/MessagePanel.cs
+++ Phyrric/Panels/MessagePanel.cs
@@ -4,27 +4,21 @@ using SadConsole.Consoles;
 
 namespace Phyrric.Panels
 {
-    public class MessagePanel : Console
-    {
+	public class MessagePanel : Console
+	{
 
-        public MessagePanel()
-            : base(Constants.MESSAGEPANEL_WIDTH - 2, Constants.MESSAGEPANEL_HEIGHT - 2)
-        {
-            Position = new Point(Constants.MESSAGEPANEL_X + 1, Constants.MESSAGEPANEL_Y + 1);
+		public MessagePanel()
+			: base(Constants.MESSAGEPANEL_WIDTH - 2, Constants.MESSAGEPANEL_HEIGHT - 2)
+		{
+			Position = new Point(Constants.MESSAGEPANEL_X + 1, Constants.MESSAGEPANEL_Y + 1);
 
-            ShiftRight(1);
-        }
+			ShiftRight(1);
+		}
 
-        public void PrintMessage(string msg)
-        {
-            ShiftDown(1);
-            VirtualCursor.Print(msg).CarriageReturn();
-        }
-
-        public void PrintMessage(ColoredString msg)
-        {
-            ShiftDown(1);
-            VirtualCursor.Print(msg).CarriageReturn();
-        }
-    }
+		public void PrintMessage(ColoredString msg)
+		{
+			ShiftDown(1);
+			VirtualCursor.Print(msg).CarriageReturn();
+		}
+	}
 }
