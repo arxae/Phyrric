e672cde94dc077f935de72dbcfd55e085e533dc5
Andy Maesen <Andy.Maesen@nike.com>
2019-11-13 05:37:36 +0100

    Initial Commit

---
+++ src/Phyrric/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Phyrric
+{
+	using Microsoft.Xna.Framework;
+	using Console = SadConsole.Console;
+
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			SadConsole.Game.Create(80, 25);
+
+			SadConsole.Game.OnInitialize = Init;
+
+			SadConsole.Game.Instance.Run();
+			SadConsole.Game.Instance.Dispose();
+		}
+
+		private static void Init()
+		{
+			Console c = new Console(80, 25);
+			c.FillWithRandomGarbage();
+			c.Fill(new Rectangle(3,3,23,3), Color.Violet, Color.Black, 0, 0);
+			c.Print(4, 4, "Ohai :D");
+
+			SadConsole.Global.CurrentScreen = c;
+		}
+	}
+}
